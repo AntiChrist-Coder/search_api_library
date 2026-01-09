5dc8d896e3033bbc351293ce7ef769b5b1cfc5a4
AntiChrist-Coder <89640235+AntiChrist-Coder@users.noreply.github.com>
2026-01-08 20:57:31 -0800

    Bump version to 2.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except UnicodeDecodeError:
 
 setup(
     name="AD-SearchAPI",
-    version="2.0.2",
+    version="2.0.3",
     packages=find_packages(),
     install_requires=[
         "requests>=2.31.0",
